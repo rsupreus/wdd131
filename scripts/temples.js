121fdef2d92ce32378c2e3eb883475abc0be4933
rsupreus <danasupreus@outlook.com>
2026-05-23 06:49:25 -0600

    fixing accessibility

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,13 +6,18 @@ new Date().getFullYear();
 
 // Display last modified date
 
-document.getElementById("lastModified").innerHTML =
-`Last Modification: ${document.lastModified}`;
-
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
 menuButton.addEventListener("click", () => {
+
     navigation.classList.toggle("open");
     menuButton.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.setAttribute("aria-label", "Close navigation menu");
+    } else {
+        menuButton.setAttribute("aria-label", "Open navigation menu");
+    }
+
 });
